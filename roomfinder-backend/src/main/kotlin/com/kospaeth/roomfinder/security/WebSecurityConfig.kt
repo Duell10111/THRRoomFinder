2ffd9d349915dc3dbf891a5718b2ff8f96a35d67
Konstantin Späth <duell10111@t-online.de>
2025-04-12 20:35:40 +0200

    chore: add gcr auth config

---
+++ roomfinder-backend/src/main/kotlin/com/kospaeth/roomfinder/security/WebSecurityConfig.kt
@@ -0,0 +1,41 @@
+package com.kospaeth.roomfinder.security
+
+import com.kospaeth.roomfinder.controller.ControllerStruct
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.invoke
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
+
+@Profile("!no-auth && !test")
+@Configuration
+class WebSecurityConfig {
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain? {
+        return http {
+            authorizeExchange {
+                authorize("${ControllerStruct.ROOM_CONTROLLER}/**", permitAll)
+                authorize(anyExchange, authenticated)
+            }
+            csrf { disable() }
+            formLogin { disable() }
+            httpBasic { disable() }
+            anonymous { disable() }
+        }
+    }
+
+    @Bean
+    fun corsConfigurationSource(): UrlBasedCorsConfigurationSource {
+        // Allow cors for local frontend app
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("https://thrroomfinder.duell10111.de")
+        configuration.allowedMethods = listOf("GET", "POST", "OPTIONS", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf("*")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
