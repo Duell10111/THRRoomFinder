be9b633eaf13b5b97491c31f35ff200a9680c324
Konstantin Späth <duell10111@t-online.de>
2025-04-21 23:41:12 +0200

    chore: add url and test dir

--- roomfinder-backend/src/konsistTest/kotlin/com/kospaeth/roomfinder/DocumentationTest.kt
+++ roomfinder-backend/src/konsistTest/kotlin/com/kospaeth/roomfinder/DocumentationTest.kt
@@ -2,4 +2,5 @@ package com.kospaeth.roomfinder
 
 class DocumentationTest {
     // TODO: Add Konsist test for KDoc documentation
+    // https://docs.konsist.lemonappdev.com/inspiration/snippets/library-snippets
 }
