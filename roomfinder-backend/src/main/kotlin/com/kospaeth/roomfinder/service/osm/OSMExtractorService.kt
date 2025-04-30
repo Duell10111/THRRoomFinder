14553d7dc4e20fe2604dab067b6a46be02234810
Konstantin Späth <duell10111@t-online.de>
2025-04-30 10:13:05 +0200

    chore: refactor error handling and holiday parsing in services
    Added error handling for OSMExtractorService and improved parsing logic for holiday events in StarPlanService. Extended test cases and mock data to validate the changes. Updated configuration with additional building mappings and admin IDs.

--- roomfinder-backend/src/main/kotlin/com/kospaeth/roomfinder/service/osm/OSMExtractorService.kt
+++ roomfinder-backend/src/main/kotlin/com/kospaeth/roomfinder/service/osm/OSMExtractorService.kt
@@ -2,10 +2,10 @@ package com.kospaeth.roomfinder.service.osm
 
 import com.kospaeth.roomfinder.config.OSMProperties
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
 import org.springframework.web.reactive.function.client.awaitExchange
 
 private val logger = KotlinLogging.logger {}
@@ -35,13 +35,12 @@ class OSMExtractorService(
 
         logger.debug { "Running query: $query" }
 
-        // TODO: Catching a error here!
         return webClient.post()
             .uri(osmProperties.overPassUrl)
             .body(BodyInserters.fromValue(query))
             .awaitExchange {
-                logger.info { it }
-                it.toEntity(OverpassResponse::class.java).awaitSingle()
-            }.body
+                logger.debug { "Received OSM Response: $it" }
+                it.awaitBody<OverpassResponse>()
+            }
     }
 }
