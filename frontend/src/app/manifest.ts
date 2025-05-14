8170cd3f32c2fd203f5b38271fd630eefd2ae81d
Konstantin Späth <duell10111@t-online.de>
2025-05-14 09:04:04 +0200

    fix: fix credits page with and manifest background and css adaptions

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -7,7 +7,7 @@ export default function manifest(): MetadataRoute.Manifest {
         description: "A Roomfinder for the TH Rosenheim",
         start_url: "/",
         display: "standalone",
-        background_color: "#ffffff",
+        background_color: "#000000",
         theme_color: "#F48530",
         icons: [
             {
