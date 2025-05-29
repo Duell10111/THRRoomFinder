08b24261934261a6b4f5f419f666690830238ea5
Konstantin Späth <duell10111@t-online.de>
2025-05-30 01:36:32 +0200

    chore: added ipad e2e tests as well

--- frontend/cypress/e2e/map-functions/map.cy.ts
+++ frontend/cypress/e2e/map-functions/map.cy.ts
@@ -1,106 +1,222 @@
 import { MapLocator } from "@mapgrab/cypress"
 
-describe("Campus Map Tests MacBook-16", () => {
-    beforeEach(() => {
-        cy.viewport("macbook-16", "portrait")
-        cy.visit(
-            "http://localhost:3000",
-            mockLocation(47.867389349999996, 12.10707175)
-        )
-    })
-
-    it("general room click and floor switch works as expected", () => {
-        cy.mapController("mainMap").then((controller) => {
-            return controller.waitToMapLoaded()
-        })
-        cy.mapLocator(
-            'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
-        )
-            .first<MapLocator>()
-            .then((locator) => locator.click())
-
-        // Check that popup appears - Content dynamic
-        cy.get(".maplibregl-popup-content")
-
-        cy.mapLocator(
-            'layer[id=highlight-room-layer] filter["==", ["get", "id"], "A0.04"]'
-        ).first<MapLocator>()
-
-        cy.get('[data-testid="roomData-name"]').contains("A0.04")
-
-        // Switch floor to 5th
-        cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger("click")
-
-        cy.mapLocator(
-            'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-        )
-            .first<MapLocator>()
-            .then((locator) => locator.click())
-
-        // Check that popup appears - Content dynamic
-        cy.get(".maplibregl-popup-content")
-
-        cy.get('[data-testid="roomData-name"]').contains("A5.04")
-    })
-
-    // it("general room click and floor switch in fullscreen", () => {
-    //     cy.mapController("mainMap").then((controller) => {
-    //         return controller.waitToMapLoaded()
-    //     })
-    //
-    //     // Enter fullscreen mode
-    //     cy.get(".maplibregl-ctrl-fullscreen").trigger("click")
-    //
-    //     cy.mapLocator(
-    //         'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
-    //     )
-    //         .first<MapLocator>()
-    //         .then((locator) => locator.click())
-    //
-    //     // Check that popup appears - Content dynamic
-    //     cy.get(".maplibregl-popup-content")
-    //
-    //     cy.get('[data-testid="roomData-name"]').contains("A0.04")
-    //
-    //     // Switch floor to 5th
-    //     cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger("click")
-    //
-    //     cy.mapLocator(
-    //         'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-    //     )
-    //         .first<MapLocator>()
-    //         .then((locator) => locator.click())
-    //
-    //     // Check that popup appears - Content dynamic
-    //     cy.get(".maplibregl-popup-content")
-    //
-    //     cy.get('[data-testid="roomData-name"]').contains("A5.04")
-    // })
-
-    it("room selection via input box", () => {
-        cy.mapController("mainMap").then((controller) => {
-            return controller.waitToMapLoaded()
-        })
-        cy.get('[data-test-id="autocomplete-input"]').type("A5.00{enter}")
-
-        cy.get('[data-testid="roomData-name"]').contains("A5.00")
-
-        cy.mapLocator(
-            'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-        ).first<MapLocator>()
-    })
-
-    it("test geolocation button works", () => {
-        cy.mapController("mainMap").then((controller) => {
-            return controller.waitToMapLoaded()
+describe("Campus Map Tests", () => {
+    context("MacBook-16", () => {
+        beforeEach(() => {
+            cy.viewport("macbook-16", "portrait")
+            cy.visit(
+                "http://localhost:3000",
+                mockLocation(47.867389349999996, 12.10707175)
+            )
         })
 
-        // Click geolocate me button
-        cy.get(".maplibregl-ctrl-geolocate").trigger("click")
+        it("general room click and floor switch works as expected", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
+            )
+                .first<MapLocator>()
+                .then((locator) => locator.click())
 
-        // Check if geolocation button is shown
-        cy.get(".maplibregl-user-location-dot")
+            // Check that popup appears - Content dynamic
+            cy.get(".maplibregl-popup-content")
+
+            cy.mapLocator(
+                'layer[id=highlight-room-layer] filter["==", ["get", "id"], "A0.04"]'
+            ).first<MapLocator>()
+
+            cy.get('[data-testid="roomData-name"]').contains("A0.04")
+
+            // Switch floor to 5th
+            cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger(
+                "click"
+            )
+
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
+            )
+                .first<MapLocator>()
+                .then((locator) => locator.click())
+
+            // Check that popup appears - Content dynamic
+            cy.get(".maplibregl-popup-content")
+
+            cy.get('[data-testid="roomData-name"]').contains("A5.04")
+        })
+
+        it("room selection via input box", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+            cy.get('[data-test-id="autocomplete-input"]').type("A5.00{enter}")
+
+            cy.get('[data-testid="roomData-name"]').contains("A5.00")
+
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
+            ).first<MapLocator>()
+        })
+
+        it("test geolocation button works", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+
+            // Click geolocate me button
+            cy.get(".maplibregl-ctrl-geolocate").trigger("click")
+
+            // Check if geolocation button is shown
+            cy.get(".maplibregl-user-location-dot")
+        })
     })
+
+    context("iPad Portrait", () => {
+        beforeEach(() => {
+            cy.viewport("ipad-2", "portrait")
+            cy.visit(
+                "http://localhost:3000",
+                mockLocation(47.867389349999996, 12.10707175)
+            )
+        })
+
+        it("general room click and floor switch works as expected", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
+            )
+                .first<MapLocator>()
+                .then((locator) => locator.click())
+
+            // Check that popup appears - Content dynamic
+            cy.get(".maplibregl-popup-content")
+
+            cy.mapLocator(
+                'layer[id=highlight-room-layer] filter["==", ["get", "id"], "A0.04"]'
+            ).first<MapLocator>()
+
+            cy.get('[data-testid="roomData-name"]').contains("A0.04")
+
+            // Switch floor to 5th
+            cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger(
+                "click"
+            )
+
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
+            )
+                .first<MapLocator>()
+                .then((locator) => locator.click())
+
+            // Check that popup appears - Content dynamic
+            cy.get(".maplibregl-popup-content")
+
+            cy.get('[data-testid="roomData-name"]').contains("A5.04")
+        })
+
+        it("room selection via input box", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+            cy.get('[data-test-id="autocomplete-input"]').type("A5.00{enter}")
+
+            cy.get('[data-testid="roomData-name"]').contains("A5.00")
+
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
+            ).first<MapLocator>()
+        })
+
+        it("test geolocation button works", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+
+            // Click geolocate me button
+            cy.get(".maplibregl-ctrl-geolocate").trigger("click")
+
+            // Check if geolocation button is shown
+            cy.get(".maplibregl-user-location-dot")
+        })
+    })
+
+    context("iPad Landscape", () => {
+        beforeEach(() => {
+            cy.viewport("ipad-2", "landscape")
+            cy.visit(
+                "http://localhost:3000",
+                mockLocation(47.867389349999996, 12.10707175)
+            )
+        })
+
+        it("general room click and floor switch works as expected", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
+            )
+                .first<MapLocator>()
+                .then((locator) => locator.click())
+
+            // Check that popup appears - Content dynamic
+            cy.get(".maplibregl-popup-content")
+
+            cy.mapLocator(
+                'layer[id=highlight-room-layer] filter["==", ["get", "id"], "A0.04"]'
+            ).first<MapLocator>()
+
+            cy.get('[data-testid="roomData-name"]').contains("A0.04")
+
+            // Switch floor to 5th
+            cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger(
+                "click"
+            )
+
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
+            )
+                .first<MapLocator>()
+                .then((locator) => locator.click())
+
+            // Check that popup appears - Content dynamic
+            cy.get(".maplibregl-popup-content")
+
+            cy.get('[data-testid="roomData-name"]').contains("A5.04")
+        })
+
+        it("room selection via input box", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+            cy.get('[data-test-id="autocomplete-input"]').type("A5.00{enter}")
+
+            cy.get('[data-testid="roomData-name"]').contains("A5.00")
+
+            cy.mapLocator(
+                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
+            ).first<MapLocator>()
+        })
+
+        it("test geolocation button works", () => {
+            cy.mapController("mainMap").then((controller) => {
+                return controller.waitToMapLoaded()
+            })
+
+            // Click geolocate me button
+            cy.get(".maplibregl-ctrl-geolocate").trigger("click")
+
+            // Check if geolocation button is shown
+            cy.get(".maplibregl-user-location-dot")
+        })
+    })
+
+    // TODO: Add iPhone Tests with check for navigator?
+
+    // TODO: Outsource duplicate code
 })
 
 function mockLocation(latitude: number, longitude: number) {
