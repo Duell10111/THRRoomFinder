4d3bafb27be0379ef601ec03e43190d070daab96
Konstantin Späth <duell10111@t-online.de>
2025-06-17 12:26:59 +0200

    feat: add spring doc annotations for openapi specification

--- roomfinder-backend/src/main/kotlin/com/kospaeth/roomfinder/controller/AdminController.kt
+++ roomfinder-backend/src/main/kotlin/com/kospaeth/roomfinder/controller/AdminController.kt
@@ -1,28 +1,51 @@
 package com.kospaeth.roomfinder.controller
 
 import com.kospaeth.roomfinder.service.AdminService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@SecurityScheme(
+    name = "Bearer Authentication",
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer",
+)
+@SecurityRequirement(name = "Bearer Authentication")
+@Tag(name = "Admin API")
 @RequestMapping(ControllerStruct.ADMIN_CONTROLLER)
 @RestController
 class AdminController(
     private val adminService: AdminService,
 ) {
+    @Operation(summary = "Delete a room from the database")
+    @ApiResponses(value = [ApiResponse(responseCode = "200", content = [Content()])])
     @DeleteMapping("/room/{roomName}")
     suspend fun deleteRoomEntry(
-        @PathVariable roomName: String,
+        @Parameter(description = "Room name to delete", required = true) @PathVariable roomName: String,
     ) {
         adminService.deleteRoomFromDatabase(roomName)
     }
 
+    @Operation(summary = "Delete all rooms from the database")
+    @ApiResponses(value = [ApiResponse(responseCode = "200", content = [Content()])])
     @DeleteMapping("/room")
     suspend fun deleteAllRooms() {
         adminService.deleteAllRoomsFromDatabase()
     }
 
+    @Operation(summary = "Clear all cached schedules")
+    @ApiResponses(value = [ApiResponse(responseCode = "200", content = [Content()])])
     @DeleteMapping("/schedules")
     suspend fun clearScheduleCache() {
         adminService.clearCachedSchedules()
--- roomfinder-backend/src/main/kotlin/com/kospaeth/roomfinder/controller/RoomController.kt
+++ roomfinder-backend/src/main/kotlin/com/kospaeth/roomfinder/controller/RoomController.kt
@@ -4,7 +4,14 @@ import com.kospaeth.roomfinder.data.dto.ExtendedRoomDTO
 import com.kospaeth.roomfinder.data.dto.RoomDTO
 import com.kospaeth.roomfinder.service.RoomService
 import com.kospaeth.roomfinder.service.splan.RoomSchedule
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,41 +19,62 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 
-@RequestMapping(ControllerStruct.ROOM_CONTROLLER)
+@Tag(name = "Rooms API")
+@RequestMapping(ControllerStruct.ROOM_CONTROLLER, produces = [APPLICATION_JSON_VALUE])
 @RestController
 class RoomController(
     private val roomService: RoomService,
 ) {
+    @Operation(summary = "Fetch all rooms stored in the database.")
     @GetMapping("/")
     suspend fun getRooms(): List<RoomDTO> {
         return roomService.getAllRooms()
     }
 
+    @Operation(
+        summary = "Fetch all rooms stored in the database associated with their building name, e.g. A0.10 -> A building, Room A0.10.",
+    )
     @GetMapping("/extended")
     suspend fun getRoomsExtended(): List<ExtendedRoomDTO> {
         return roomService.getAllRoomsWithBuildings()
     }
 
+    @Operation(summary = "Get location of the specified room.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200"),
+            ApiResponse(
+                responseCode = "404",
+                description = "No room found with the specified name.",
+                content =
+                    arrayOf(
+                        Content(),
+                    ),
+            ),
+        ],
+    )
     @GetMapping("/{roomName}")
     suspend fun getLocationForRoom(
-        @PathVariable roomName: String,
+        @Parameter(description = "Room name to fetch location", required = true) @PathVariable roomName: String,
     ): ResponseEntity<RoomDTO> {
         return roomService.getLocationForRoom(roomName)?.let {
             ResponseEntity.ok(it)
         } ?: ResponseEntity.notFound().build()
     }
 
+    @Operation(summary = "Get schedules in the area of the room specified and all rooms available in the cache.")
     @GetMapping("/{roomName}/schedule/related")
     suspend fun getScheduleForRoomRelated(
-        @PathVariable roomName: String,
+        @Parameter(description = "Room name to use as base for related schedules", required = true) @PathVariable roomName: String,
     ): ResponseEntity<Map<String, List<RoomSchedule>>> {
         return roomService.getRelatedRoomScheduleForRoom(roomName = roomName)
             .let { ResponseEntity.ok(it.mapValues { it.value.schedule }) }
     }
 
+    @Operation(summary = "Get schedules of the room specified for the current week, starting from Monday.")
     @GetMapping("/{roomName}/schedule")
     suspend fun getScheduleForRoom(
-        @PathVariable roomName: String,
+        @Parameter(description = "Room name to fetch schedule for", required = true) @PathVariable roomName: String,
     ): ResponseEntity<List<RoomSchedule>> {
         return roomService.getRoomScheduleForRoom(roomName).let {
             ResponseEntity.ok()
@@ -55,10 +83,12 @@ class RoomController(
         }
     }
 
+    @Operation(summary = "Get schedules of the room specified for the week of the specified date, starting from Monday.")
     @GetMapping("/{roomName}/schedule/{date}")
     suspend fun getScheduleForRoomForDate(
-        @PathVariable roomName: String,
-        @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") date: LocalDate,
+        @Parameter(description = "Room name to fetch schedule for", required = true) @PathVariable roomName: String,
+        @Parameter(description = "Date to fetch schedule for", required = true) @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") date:
+            LocalDate,
     ): ResponseEntity<List<RoomSchedule>> {
         return roomService.getRoomScheduleForRoom(roomName, date).let { ResponseEntity.ok(it.schedule) }
     }
