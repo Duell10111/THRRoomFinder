bd5bb4c90da52b1a55070b4577cdb9cab50cab94
Konstantin Späth <duell10111@t-online.de>
2025-06-20 18:38:48 +0200

    chore: remove duplicated e2e tests for different device types

--- frontend/cypress/e2e/map-functions/map.cy.ts
+++ frontend/cypress/e2e/map-functions/map.cy.ts
@@ -1,223 +1,73 @@
 import { MapLocator } from "@mapgrab/cypress"
 
 describe("Campus Map Tests", () => {
-    context("MacBook-16", () => {
-        beforeEach(() => {
-            cy.viewport("macbook-16", "portrait")
-            cy.visit(
-                "http://localhost:3000",
-                mockLocation(47.867389349999996, 12.10707175)
-            )
-        })
-
-        it("general room click and floor switch works as expected", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
-            )
-                .first<MapLocator>()
-                .then((locator) => locator.click())
-
-            // Check that popup appears - Content dynamic
-            cy.get(".maplibregl-popup-content")
-
-            cy.mapLocator(
-                'layer[id=highlight-room-layer] filter["==", ["get", "id"], "A0.04"]'
-            ).first<MapLocator>()
-
-            cy.get('[data-testid="roomData-name"]')
-                .contains("A0.04")
-                .should("be.visible")
-
-            // Switch floor to 5th
-            cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger(
-                "click"
-            )
-
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-            )
-                .first<MapLocator>()
-                .then((locator) => locator.click())
-
-            // Check that popup appears - Content dynamic
-            cy.get(".maplibregl-popup-content")
-
-            cy.get('[data-testid="roomData-name"]').contains("A5.04")
-        })
-
-        it("room selection via input box", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-            cy.get('[data-testid="autocomplete-input"]').type("A5.00{enter}")
-
-            cy.get('[data-testid="roomData-name"]').contains("A5.00")
-
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-            ).first<MapLocator>()
-        })
-
-        it("test geolocation button works", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-
-            // Click geolocate me button
-            cy.get(".maplibregl-ctrl-geolocate").trigger("click")
-
-            // Check if geolocation button is shown
-            cy.get(".maplibregl-user-location-dot")
-        })
+    beforeEach(() => {
+        cy.viewport("macbook-16", "portrait")
+        cy.visit(
+            "http://localhost:3000",
+            mockLocation(47.867389349999996, 12.10707175)
+        )
     })
 
-    context("iPad Portrait", () => {
-        beforeEach(() => {
-            cy.viewport("ipad-2", "portrait")
-            cy.visit(
-                "http://localhost:3000",
-                mockLocation(47.867389349999996, 12.10707175)
-            )
+    it("general room click and floor switch works as expected", () => {
+        cy.mapController("mainMap").then((controller) => {
+            return controller.waitToMapLoaded()
         })
+        cy.mapLocator(
+            'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
+        )
+            .first<MapLocator>()
+            .then((locator) => locator.click())
 
-        it("general room click and floor switch works as expected", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
-            )
-                .first<MapLocator>()
-                .then((locator) => locator.click())
+        // Check that popup appears - Content dynamic
+        cy.get(".maplibregl-popup-content")
 
-            // Check that popup appears - Content dynamic
-            cy.get(".maplibregl-popup-content")
+        cy.mapLocator(
+            'layer[id=highlight-room-layer] filter["==", ["get", "id"], "A0.04"]'
+        ).first<MapLocator>()
 
-            cy.mapLocator(
-                'layer[id=highlight-room-layer] filter["==", ["get", "id"], "A0.04"]'
-            ).first<MapLocator>()
+        cy.get('[data-testid="roomData-name"]')
+            .contains("A0.04")
+            .should("be.visible")
 
-            cy.get('[data-testid="roomData-name"]')
-                .contains("A0.04")
-                .should("be.visible")
+        // Switch floor to 5th
+        cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger("click")
 
-            // Switch floor to 5th
-            cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger(
-                "click"
-            )
+        cy.mapLocator(
+            'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
+        )
+            .first<MapLocator>()
+            .then((locator) => locator.click())
 
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-            )
-                .first<MapLocator>()
-                .then((locator) => locator.click())
+        // Check that popup appears - Content dynamic
+        cy.get(".maplibregl-popup-content")
 
-            // Check that popup appears - Content dynamic
-            cy.get(".maplibregl-popup-content")
-
-            cy.get('[data-testid="roomData-name"]').contains("A5.04")
-        })
-
-        it("room selection via input box", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-            cy.get('[data-testid="autocomplete-input"]').type("A5.00{enter}")
-
-            cy.get('[data-testid="roomData-name"]').contains("A5.00")
-
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-            ).first<MapLocator>()
-        })
-
-        it("test geolocation button works", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-
-            // Click geolocate me button
-            cy.get(".maplibregl-ctrl-geolocate").trigger("click")
-
-            // Check if geolocation button is shown
-            cy.get(".maplibregl-user-location-dot")
-        })
+        cy.get('[data-testid="roomData-name"]').contains("A5.04")
     })
 
-    context("iPad Landscape", () => {
-        beforeEach(() => {
-            cy.viewport("ipad-2", "landscape")
-            cy.visit(
-                "http://localhost:3000",
-                mockLocation(47.867389349999996, 12.10707175)
-            )
+    it("room selection via input box", () => {
+        cy.mapController("mainMap").then((controller) => {
+            return controller.waitToMapLoaded()
+        })
+        cy.get('[data-testid="autocomplete-input"]').type("A5.00{enter}")
+
+        cy.get('[data-testid="roomData-name"]').contains("A5.00")
+
+        cy.mapLocator(
+            'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
+        ).first<MapLocator>()
+    })
+
+    it("test geolocation button works", () => {
+        cy.mapController("mainMap").then((controller) => {
+            return controller.waitToMapLoaded()
         })
 
-        it("general room click and floor switch works as expected", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A0.04"]'
-            )
-                .first<MapLocator>()
-                .then((locator) => locator.click())
+        // Click geolocate me button
+        cy.get(".maplibregl-ctrl-geolocate").trigger("click")
 
-            // Check that popup appears - Content dynamic
-            cy.get(".maplibregl-popup-content")
-
-            cy.mapLocator(
-                'layer[id=highlight-room-layer] filter["==", ["get", "id"], "A0.04"]'
-            ).first<MapLocator>()
-
-            cy.get('[data-testid="roomData-name"]')
-                .contains("A0.04")
-                .should("be.visible")
-
-            // Switch floor to 5th
-            cy.get(".maplibregl-ctrl-indoorequal > :nth-child(1)").trigger(
-                "click"
-            )
-
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-            )
-                .first<MapLocator>()
-                .then((locator) => locator.click())
-
-            // Check that popup appears - Content dynamic
-            cy.get(".maplibregl-popup-content")
-
-            cy.get('[data-testid="roomData-name"]').contains("A5.04")
-        })
-
-        it("room selection via input box", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-            cy.get('[data-testid="autocomplete-input"]').type("A5.00{enter}")
-
-            cy.get('[data-testid="roomData-name"]').contains("A5.00")
-
-            cy.mapLocator(
-                'layer[id=indoor-name] filter["==", ["get", "name"], "A5.04"]'
-            ).first<MapLocator>()
-        })
-
-        it("test geolocation button works", () => {
-            cy.mapController("mainMap").then((controller) => {
-                return controller.waitToMapLoaded()
-            })
-
-            // Click geolocate me button
-            cy.get(".maplibregl-ctrl-geolocate").trigger("click")
-
-            // Check if geolocation button is shown
-            cy.get(".maplibregl-user-location-dot")
-        })
+        // Check if geolocation button is shown
+        cy.get(".maplibregl-user-location-dot")
     })
 })
 
--- frontend/cypress/e2e/navigation.cy.ts
+++ frontend/cypress/e2e/navigation.cy.ts
@@ -1,114 +1,23 @@
 describe("Navigation Tests", () => {
-    context("MacBook-16", () => {
-        beforeEach(() => {
-            cy.viewport("macbook-16", "portrait")
-            try {
-                cy.visit("http://localhost:3000")
-            } catch (e) {
-                console.error(e)
-            }
-        })
-        it("NavBar can be opened and closed correctly", () => {
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "be.visible"
-            )
-
-            cy.get('[data-testid="nav-burger-desktop"]').click()
-
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "not.be.visible"
-            )
-
-            cy.get('[data-testid="nav-burger-desktop"]').click()
-
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "be.visible"
-            )
-        })
+    beforeEach(() => {
+        cy.viewport("macbook-16", "portrait")
+        try {
+            cy.visit("http://localhost:3000")
+        } catch (e) {
+            console.error(e)
+        }
     })
+    it("NavBar can be opened and closed correctly", () => {
+        cy.get('[data-testid="room-details-no-selection"]').should("be.visible")
 
-    context("iPad Portrait", () => {
-        beforeEach(() => {
-            cy.viewport("ipad-2", "portrait")
-            try {
-                cy.visit("http://localhost:3000")
-            } catch (e) {
-                console.error(e)
-            }
-        })
-        it("NavBar can be opened and closed correctly", () => {
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "be.visible"
-            )
+        cy.get('[data-testid="nav-burger-desktop"]').click()
 
-            cy.get('[data-testid="nav-burger-desktop"]').click()
+        cy.get('[data-testid="room-details-no-selection"]').should(
+            "not.be.visible"
+        )
 
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "not.be.visible"
-            )
+        cy.get('[data-testid="nav-burger-desktop"]').click()
 
-            cy.get('[data-testid="nav-burger-desktop"]').click()
-
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "be.visible"
-            )
-        })
+        cy.get('[data-testid="room-details-no-selection"]').should("be.visible")
     })
-
-    context("iPad Landscape", () => {
-        beforeEach(() => {
-            cy.viewport("ipad-2", "landscape")
-            try {
-                cy.visit("http://localhost:3000")
-            } catch (e) {
-                console.error(e)
-            }
-        })
-        it("NavBar can be opened and closed correctly", () => {
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "be.visible"
-            )
-
-            cy.get('[data-testid="nav-burger-desktop"]').click()
-
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "not.be.visible"
-            )
-
-            cy.get('[data-testid="nav-burger-desktop"]').click()
-
-            cy.get('[data-testid="room-details-no-selection"]').should(
-                "be.visible"
-            )
-        })
-    })
-
-    // context("iPhone Portrait", () => {
-    //     beforeEach(() => {
-    //         cy.viewport("iphone-xr", "portrait")
-    //         try {
-    //             cy.visit("http://localhost:3000")
-    //         } catch (e) {
-    //             console.error(e)
-    //         }
-    //     })
-    //     it("NavBar can be opened and closed correctly", () => {
-    //         // Not visible on default on iPhone
-    //         cy.get('[data-testid="room-details-no-selection"]').should(
-    //             "not.be.visible"
-    //         )
-    //
-    //         cy.get('[data-testid="nav-burger-mobile"]').click()
-    //
-    //         cy.get('[data-testid="room-details-no-selection"]').should(
-    //             "be.visible"
-    //         )
-    //
-    //         cy.get('[data-testid="nav-burger-mobile"]').click()
-    //
-    //         cy.get('[data-testid="room-details-no-selection"]').should(
-    //             "not.be.visible"
-    //         )
-    //     })
-    // })
 })
