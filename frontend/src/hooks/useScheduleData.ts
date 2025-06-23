9b1cf2eec0b247ef7f5b8682d06de332e72c7c2a
Konstantin Späth <duell10111@t-online.de>
2025-06-23 17:29:00 +0200

    chore: outsource fetch schedule data

--- frontend/src/hooks/useScheduleData.ts
+++ frontend/src/hooks/useScheduleData.ts
@@ -1,18 +1,12 @@
-import {useEffect, useState} from "react";
-import {backendUrl} from "@/utils/const";
-import {ScheduleData} from "@/utils/data";
-
+import { useEffect, useState } from "react"
+import { fetchScheduleData, ScheduleData } from "@/utils/data"
 
 export default function useScheduleData(roomName: string) {
-    const [scheduleData, setScheduleData] = useState<ScheduleData[]>();
+    const [scheduleData, setScheduleData] = useState<ScheduleData[]>()
 
     useEffect(() => {
-        async function fetchScheduleData() {
-            const data = await fetch(`${backendUrl}/api/v1/room/${roomName}/schedule`)
-            return await data.json() as ScheduleData[]
-        }
-        fetchScheduleData().then(setScheduleData).catch(console.warn)
+        fetchScheduleData(roomName).then(setScheduleData).catch(console.warn)
     }, [roomName])
 
-    return scheduleData;
+    return scheduleData
 }
